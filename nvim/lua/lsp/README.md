ce195fb918d15196c588e03b8301bb22bf5777af
Mike Henning <henning@fh-aachen.de>
2022-10-19 22:19:40 +0200

    nvim: added frontend and node capabilities

---
+++ nvim/lua/lsp/README.md
@@ -0,0 +1,1 @@
+Everything in this folder and subfolders was yanked from "https://github.com/ecosse3/nvim", thanks my dude
--- nvim/lua/mhenn/lsp.lua
+++ nvim/lua/mhenn/lsp.lua
@@ -1,6 +1,6 @@
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-
+local typescript_ok, typescript = pcall(require, 'typescript')
 require'lspconfig'.clangd.setup{}
 
 local opts = { noremap=true, silent=true }
@@ -40,7 +40,9 @@ local lsp_flags = {
   debounce_text_changes = 150,
 }
 
-require('lspconfig')['rust_analyzer'].setup{
+local lspconfig = require("lspconfig")
+
+lspconfig.rust_analyzer.setup{
     on_attach = on_attach,
     flags = lsp_flags,
     -- Server-specific settings...
@@ -61,24 +63,101 @@ require('lspconfig')['rust_analyzer'].setup{
     }
 }
 
-vim.g.markdown_fenced_languages = {
-  "ts=typescript"
+--
+--"bash-language-server", "css-lsp", "eslint-lsp", "html-lsp",
+--    "json-lsp", "lua-language-server", "tailwindcss-language-server", "typescript-language-server",
+--    "vue-language-server",  "node-debug2-adapter"
+--
+
+
+-- It enables tsserver automatically so no need to call lspconfig.tsserver.setup
+if typescript_ok then
+  typescript.setup({
+    disable_commands = false, -- prevent the plugin from creating Vim commands
+    debug = false, -- enable debug logging for commands
+    -- LSP Config options
+    server = {
+      capabilities = require('lsp.servers.tsserver').capabilities,
+      handlers = require('lsp.servers.tsserver').handlers,
+      on_attach = require('lsp.servers.tsserver').on_attach,
+      settings = require('lsp.servers.tsserver').settings,
+    }
+  })
+end
+
+lspconfig.tailwindcss.setup {
+  capabilities = require('lsp.servers.tailwindcss').capabilities,
+  filetypes = require('lsp.servers.tailwindcss').filetypes,
+  handlers = handlers,
+  init_options = require('lsp.servers.tailwindcss').init_options,
+  on_attach = require('lsp.servers.tailwindcss').on_attach,
+  settings = require('lsp.servers.tailwindcss').settings,
 }
 
-require'lspconfig'.denols.setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
+lspconfig.cssls.setup {
+  capabilities = capabilities,
+  handlers = handlers,
+  on_attach = require('lsp.servers.cssls').on_attach,
+  settings = require('lsp.servers.cssls').settings,
 }
 
-require('lspconfig')['clangd'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
+lspconfig.eslint.setup {
+  capabilities = capabilities,
+  handlers = handlers,
+  on_attach = require('lsp.servers.eslint').on_attach,
+  settings = require('lsp.servers.eslint').settings,
 }
 
-require('lspconfig')['texlab'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
+lspconfig.jsonls.setup {
+  capabilities = capabilities,
+  handlers = handlers,
+  on_attach = on_attach,
+  settings = require('lsp.servers.jsonls').settings,
 }
+
+lspconfig.sumneko_lua.setup {
+  capabilities = capabilities,
+  handlers = handlers,
+  on_attach = on_attach,
+  settings = require('lsp.servers.sumneko_lua').settings,
+}
+
+lspconfig.vuels.setup {
+  filetypes = require('lsp.servers.vuels').filetypes,
+  handlers = handlers,
+  init_options = require('lsp.servers.vuels').init_options,
+  on_attach = on_attach,
+}
+
+for _, server in ipairs { "bashls" , "html" } do
+  lspconfig[server].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    handlers = handlers,
+  }
+end
+
+
+
+
+--vim.g.markdown_fenced_languages = {
+--  "ts=typescript"
+--}
+--
+--require'lspconfig'.denols.setup{
+--    on_attach = on_attach,
+--    flags = lsp_flags,
+--}
+--
+--require('lspconfig')['clangd'].setup{
+--    on_attach = on_attach,
+--    flags = lsp_flags,
+--}
+--
+--require('lspconfig')['texlab'].setup{
+--    on_attach = on_attach,
+--    flags = lsp_flags,
+--}
 
 ----------------------------------------------------
 --
